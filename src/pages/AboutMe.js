70ea2195fd289b839061e31185db529e56c6311c
ant1010 <arreola.antony@gmail.com>
2022-06-08 02:03:46 -0600

    first

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./AboutMe.css";
+
+
+function AboutMe({}) {
+    return (
+       
+        <div className="page about-container">
+           <div className = "bubble">
+           <h2>About</h2>
+           </div>
+            <div className = "moon"></div>
+        </div>
+            
+      
+    )
+}
+
+export default AboutMe;
