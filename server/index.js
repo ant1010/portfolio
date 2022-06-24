123b4d15bcee58d45337d82d01802167fb2d869e
ant1010 <arreola.antony@gmail.com>
2022-06-23 19:13:19 -0600

    heroku port fix

--- server/index.js
+++ server/index.js
@@ -59,7 +59,6 @@ app.post('/send', (req, res, next) => {
 })
 
 
-const port = 3000;
-app.listen(port, () => {
- console.log(`Server is running on port: ${port}`);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  })
